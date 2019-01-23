3df28c755c3d0785c8b86037b08027fffc15c337
Anthony Rodriguez <anthony_rodriguez@berkeley.edu>
2019-01-22 16:53:23 -0800

    needed to anchor with no href in url

---
+++ scripts/portfolio.js
@@ -0,0 +1,11 @@
+document.getElementById("about-anchor").addEventListener("click", function () {
+    document.getElementById("about-section").scrollIntoView();
+})
+
+document.getElementById("projects-anchor").addEventListener("click", function () {
+    document.getElementById("projects-section").scrollIntoView();
+})
+
+document.getElementById("contact-anchor").addEventListener("click", function () {
+    document.getElementById("contact-section").scrollIntoView();
+})

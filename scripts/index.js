3e32aa855b660f51bd0de87fbf69774015569829
Anthony Rodriguez <anthony_rodriguez@berkeley.edu>
2019-01-22 17:12:32 -0800

    for anchoring correctly

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+document.getElementById("projects-anchor").addEventListener("click", function () {
+    window.location.href = 'https://arod209.github.io/portfolio.html'
+    document.getElementById("projects-section").scrollIntoView();
+})
+
+document.getElementById("contact-anchor").addEventListener("click", function () {
+    window.location.href = 'https://arod209.github.io/portfolio.html'
+    document.getElementById("contact-section").scrollIntoView();
+})

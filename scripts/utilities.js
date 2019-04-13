9886c9245f297f99a822c5d6350a725ba522fb5c
Anthony Rodriguez <anthony_rodriguez@berkeley.edu>
2019-04-13 07:04:48 -0700

    bug fix

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,7 +1,7 @@
 /* ***** Utility functions ***** */
 
 // Returns Euclidean distance
-function distance(x1, y1, x2, y2) {
+export function distance(x1, y1, x2, y2) {
     let xDistance = x2 - x1;
     let yDistance = y2 - y1;
     return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
